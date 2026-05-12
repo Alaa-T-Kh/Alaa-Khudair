59f347d8fc5a71c8f0074e1e6d2edd01d7e8bc5a
Alaa T Khudair <alaatayseer2003@gmail.com>
2026-05-13 00:41:15 +0300

    H.W 1

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,25 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/about', function () {
+    $name = 'Alaa Khudair';
+
+    $departments = [
+        '1' => 'Tichnical',
+        '2' => 'Financial',
+        '3' => 'Sales',
+        ];
+    //return view('about')->with('name', $name);
+    // return view('about', ['name' => $name]);
+    return view('about', Compact('name' ,'departments'));
+});
+Route::post('/about', function () {
+    $name = $_POST['name'];
+    $departments = [
+        '1' => 'Tichnical',
+        '2' => 'Financial',
+        '3' => 'Sales',
+        ];
+    return view('about', Compact('name','departments'));
+
+});
