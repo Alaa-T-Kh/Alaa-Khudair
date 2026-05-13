e3c17814c0e31052675e26f2a3974dfb32205271
Alaa T Khudair <alaatayseer2003@gmail.com>
2026-05-14 00:47:35 +0300

    H.W 2

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -25,5 +26,16 @@ Route::post('/about', function () {
         '3' => 'Sales',
         ];
     return view('about', Compact('name','departments'));
+
+});
+Route::get('tasks', function () {
+    return view('tasks');
+});
+Route::post('create', function () {
+    $task_name = $_POST['name'];
+    DB::table(table:'tasks')->insert([
+        'name' => $task_name,
+    ]);
+    return view('tasks');
 
 });
